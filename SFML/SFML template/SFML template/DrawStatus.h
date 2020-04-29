34b03cd30648fc4d80d69549c1762aec9a4285b5
Jeremy <jsow1997@gmail.com>
2020-04-29 21:07:22 +1000

    changed files

--- SFML/SFML template/SFML template/DrawStatus.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-
-//struct to determine which status is the entity in
-struct drawstatus {
-	bool initializeEntity;
-	bool drawingEntity;
-	bool drawnEntity;		
-};
--- SFML/SFML template/SFML template/Entity.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <SFML\Graphics.hpp>
-#include "Toolbar.h"
-
-struct Entity {		//base struct for shape and paint. botth shape and paint inherits from entity.
-	virtual void drawEntity(sf::RenderWindow& createwindow, const Toolbar& toolbar) = 0;	
-	virtual void resize(const sf::Mouse& mouse, sf::RenderWindow& createwindow, const Toolbar& toolbar) = 0;	//resize the entity (pant brush size,shape size etc).
-};
